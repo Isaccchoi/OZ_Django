149ab17dd1fd2393fe17b85b346f9b554f7d99c0
isaccchoi <isaccchoi5@gmail.com>
2024-04-10 19:25:28 +0900

    2-9 미니 프로젝트 북마크 목록 페이지에 ORM 활용하기

--- bookmark/bookmark/views.py
+++ bookmark/bookmark/views.py
@@ -5,7 +5,7 @@ from bookmark.models import Bookmark
 
 
 def bookmark_list(request):
-    bookmarks = Bookmark.objects.all()
+    bookmarks = Bookmark.objects.filter(id__gte=50)
     # SELECT * FROM bookmark
 
     context = {
@@ -26,3 +26,17 @@ def bookmark_detail(request, pk):
 
     context = {'bookmark': bookmark}
     return render(request, 'bookmark_detail.html', context)
+
+
+################
+# Mini Project #
+################
+
+# for i in range(10):
+#     Bookmark.objects.create(name=f'테스트 네이버 {i}', url=f'https://naver.com')
+
+# bookmark_list = [Bookmark(name=f'테스트 구글 {i}', url=f'https://google.com') for i in range(10)]
+# Bookmark.objects.bulk_create(bookmark_list)
+# new_bookmark_list = [Bookmark(name=f'테스트 야후 {i}', url=f'https://yahoo.com') for i in range(80)]
+# Bookmark.objects.bulk_create(new_bookmark_list)
+# Bookmark.objects.count()
