eac93c78b6f462a73392089af07e3ef1cbcc1687
isaccchoi <isaccchoi5@gmail.com>
2024-04-13 17:22:06 +0900

    3-4 쿠키와 세션

--- blog/blog/views.py
+++ blog/blog/views.py
@@ -16,3 +16,23 @@ def blog_detail(request, pk):
     blog = get_object_or_404(Blog, pk=pk)
     context = {'blog': blog}
     return render(request, 'blog_detail.html', context)
+
+
+# 쿠키와 세션에서 사용한 blog_list
+# def blog_list_with_cookie_and_session(request):
+#     blogs = Blog.objects.all()
+#
+#     visits = int(request.COOKIES.get('visits', 0)) + 1
+#
+#     request.session['count'] = request.session.get('count', 0) + 1
+#
+#     context = {
+#         'blogs': blogs,
+#         'count': request.session['count']
+#     }
+#
+#     response = render(request, 'blog_list.html', context)
+#
+#     response.set_cookie('visits', visits)
+#
+#     return response
