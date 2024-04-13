d5d51f9f2ba5b370309b4cd30c09ba0bed11b96b
isaccchoi <isaccchoi5@gmail.com>
2024-04-13 19:03:00 +0900

    3-5 Login 페이지 만들기

--- blog/config/urls.py
+++ blog/config/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 from blog import views
 
@@ -23,4 +23,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.blog_list, name="blog_list"),
     path('<int:pk>/', views.blog_detail, name="blog_detail"),
+    path('accounts/', include("django.contrib.auth.urls")),
 ]
