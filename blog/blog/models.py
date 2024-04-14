c7b29f6bdb8732ff1b695fdd525c757ef1ad4d04
isaccchoi <isaccchoi5@gmail.com>
2024-04-14 17:01:26 +0900

    4-1 블로그에 작성자 컬럼 만들기 및 오류 해결법

--- blog/blog/models.py
+++ blog/blog/models.py
@@ -1,4 +1,7 @@
+from django.contrib.auth import get_user_model
 from django.db import models
+
+User = get_user_model()
 
 
 class Blog(models.Model):
@@ -11,6 +14,10 @@ class Blog(models.Model):
     category = models.CharField('카테고리', max_length=10, choices=CATEGORY_CHOICES)
     title = models.CharField('제목', max_length=100)
     content = models.TextField('본문')
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    # models.CASCADE => 같이 삭제
+    # models.PROTECT => 삭제가 불가능함 (유저를 삭제하려고할때 블로그가 있으면 유저 삭제가 불가능)
+    # models.SET_NULL => 널값을 넣습니다. => 유저 삭제시 블로그의 author가 null이 됨
 
     created_at = models.DateTimeField('작성일자', auto_now_add=True)
     updated_at = models.DateTimeField('수정일자', auto_now=True)
