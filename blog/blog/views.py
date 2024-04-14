faa0c68e2fa21878fccf86f08fd66c00d01675fc
isaccchoi <isaccchoi5@gmail.com>
2024-04-14 20:10:40 +0900

    4-6 블로그 페이지네이션

--- blog/blog/views.py
+++ blog/blog/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator
 from django.http import Http404
 from django.shortcuts import get_object_or_404, redirect, render
 from django.urls import reverse
@@ -10,8 +11,13 @@ from blog.models import Blog
 def blog_list(request):
     blogs = Blog.objects.all().order_by('-created_at')
 
+    paginator = Paginator(blogs, 10)
+    page = request.GET.get('page')
+    page_object = paginator.get_page(page)
+
     context = {
-        'blogs': blogs
+        # 'blogs': blogs,
+        'page_object': page_object,
     }
     return render(request, 'blog_list.html', context)
 
