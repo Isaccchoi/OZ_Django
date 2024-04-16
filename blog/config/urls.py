c479b405013038b3764ee5ceae584be3912c90a1
isaccchoi <isaccchoi5@gmail.com>
2024-04-16 22:38:18 +0900

    5-1 Class Based View

--- blog/config/urls.py
+++ blog/config/urls.py
@@ -15,11 +15,27 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, path
+from django.shortcuts import redirect, render
+from django.urls import include, path, reverse
+from django.views import View
+from django.views.generic import RedirectView, TemplateView
 
 from blog import views
 
 from member import views as member_views
+
+
+class AboutView(TemplateView):
+    template_name = 'about.html'
+
+
+class TestView(View):
+    def get(self, request):
+        return render(request, 'test_get.html')
+
+    def post(self, request):
+        return render(request, 'test_post.html')
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -33,4 +49,11 @@ urlpatterns = [
     path('accounts/', include("django.contrib.auth.urls")),
     path('signup/', member_views.sign_up, name='signup'),
     path('login/', member_views.login, name='login'),
+
+
+    # path('about/', TemplateView.as_view(template_name='about.html'), name='about'),
+    path('about/', AboutView.as_view(), name='about'),
+    path('redirect/', RedirectView.as_view(pattern_name='about'), name='redirect'),
+    path('test/', TestView.as_view(), name='test'),
+    # path('redirect2/', lambda req: redirect(reverse('about'))),
 ]
