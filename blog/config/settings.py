e74fb694ccb74ab1508701522261e1f932a7daf6
isaccchoi <isaccchoi5@gmail.com>
2024-04-20 18:52:01 +0900

    6-1 Django Static File

--- blog/config/settings.py
+++ blog/config/settings.py
@@ -127,6 +127,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_DIR = BASE_DIR / 'static'
+
+STATICFILES_DIRS = [
+    STATIC_DIR,
+]
+
+STATIC_ROOT = BASE_DIR / '.static_root'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
