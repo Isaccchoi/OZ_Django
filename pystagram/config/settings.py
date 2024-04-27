11c74876146919e0cbddc085e0655b1e72d37a24
isaccchoi <isaccchoi5@gmail.com>
2024-04-27 20:48:27 +0900

    8-5 환경변수 관리

--- pystagram/config/settings.py
+++ pystagram/config/settings.py
@@ -9,18 +9,25 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+import json
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+with open(BASE_DIR / '.config_secret' / 'secret.json') as f:
+    config_secret_str = f.read()
+
+
+SECRET = json.loads(config_secret_str)
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-vle^o0+m@lcs^ecllap!)&cc^rf1wgg31n$34at%p&2o54auoq'
+SECRET_KEY = SECRET['DJANGO_SECRET_KEY']
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
