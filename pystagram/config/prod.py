a690e59c1a9967001011a90b29c7bed0a010bc5e
isaccchoi <isaccchoi5@gmail.com>
2024-06-02 16:20:13 +0900

    feat: DB 설정

--- pystagram/config/prod.py
+++ pystagram/config/prod.py
@@ -2,3 +2,14 @@ from .base import *
 DEBUG = True
 
 ALLOWED_HOSTS = ['3.36.114.50', 'oz.isaccchoi.com',]
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": SECRET['db']['name'],
+        "USER": SECRET['db']['user'],
+        "PASSWORD": SECRET['db']['password'],
+        "HOST": SECRET['db']['host'],
+        "PORT": "5432"
+    }
+}
